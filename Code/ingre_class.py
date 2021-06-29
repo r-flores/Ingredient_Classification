b8a552b69c39e16db855930f8dee138aa4b70884
r-flores <55996723+r-flores@users.noreply.github.com>
2021-06-29 10:56:03 -0500

    Comments

---
+++ Code/ingre_class.py
@@ -0,0 +1,3 @@
+# Download a subset of data from Food ingre DB FoodData central (USDA)
+# Gether additional data from FoodON (food Ontology)
+# Compare and return terms that would be considered ingredients by comparsion (If it exist in FoodOn its probably an ingrdient)
