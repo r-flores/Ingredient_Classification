4482babbce2a7cbd4a31cbce77d21800ec2fd7ff
r-flores <55996723+r-flores@users.noreply.github.com>
2021-06-30 00:52:20 -0500

    added close

--- Code/ingre_class.py
+++ Code/ingre_class.py
@@ -2,8 +2,9 @@ import csv
 # Download a subset of data from Food ingre DB FoodData central (USDA)
 ## read csv in to a dictonary
 with open('Code\\testset.csv') as data:
-    reader = csv.DictReader(data)
+    reader = csv.reader(data)
     for row in reader:
         print(row)
+data.close()
 # Gether additional data from FoodON (food Ontology)
 # Compare and return terms that would be considered ingredients by comparsion (If it exist in FoodOn its probably an ingrdient)
