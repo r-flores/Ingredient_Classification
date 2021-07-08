95168b2a0fb79d2a85a4a610006e54310cca41bb
Ricky <rickyflores@unomaha.edu>
2021-07-08 17:26:25 -0500

    Fixed csv

--- Code/ingre_class.py
+++ Code/ingre_class.py
@@ -42,7 +42,8 @@ def ontology_classifier(filename):
 
     # create a new files for writing results
     classified_terms = open("classified_Terms.csv", 'w')
-    classified_terms.write("term,membership\n")
+    write = csv.DictWriter(classified_terms, fieldnames=['Term', 'Membership'])
+    write.writeheader()
 
     # collection of already examined words
     tested_terms = []
@@ -69,11 +70,11 @@ def ontology_classifier(filename):
             for t in word_list:
                 if t not in tested_terms:
                     tested_terms.append(t)
-                    classified_terms.write(str(t))
+                    #classified_terms.write(str(t))
 
                     # Search for the term exactly as is.
                     term_class = food_onto.search_one(label=t)
-                    classified_terms.write(",")
+                    #classified_terms.write(",")
                     try:
 
                         # All FoodOn properties https://www.ebi.ac.uk/ols/ontologies/foodon/properties
@@ -81,13 +82,15 @@ def ontology_classifier(filename):
                         # between a item and a collection.
 
                         #classified_terms.write(str(term_class.RO_0002350))
-                        classified_terms.write(str(food_onto.search_one(is_a=term_class.RO_0002350).label))
+                        write.writerow({'Term': str(t), 'Membership': str(food_onto.search_one(is_a=term_class.RO_0002350).label)})
+                        #classified_terms.write(str(food_onto.search_one(is_a=term_class.RO_0002350).label))
                         # classified_terms.write(str(term_class.label))
                     except AttributeError as e:
-                        classified_terms.write("No Class\n")
+                        #classified_terms.write("No Class\n")
+                        write.writerow({'Term': str(t), 'Membership': 'No Class'})
                         continue
 
-                    classified_terms.write("\n")
+                    #classified_terms.write("\n")
                 else:
                     print(str(t) + " is duplicate")
 
